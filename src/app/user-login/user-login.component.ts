0372290d5a928cbc162191f52fea0da1b488eb73
KartikPatelOfficial <patelkartik1910@gmail.com>
2019-03-05 18:39:10 +0530

    Add: email password sign in.

--- src/app/user-login/user-login.component.ts
+++ src/app/user-login/user-login.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit, ViewChild} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {AuthService} from '../core/auth.service';
 import {FormControl, Validators} from '@angular/forms';
 
